e7672163443cc9ae5a5da89afe49167e56f63241
InterAl <interal@gmail.com>
2017-02-09 21:31:49 +0200

    tests description

--- test/UnionIntervals.test.js
+++ test/UnionIntervals.test.js
@@ -2,7 +2,7 @@ import {unionIntervals as union} from '../src/index';
 import {expect} from 'chai';
 
 describe('unionIntervals', () => {
-    it('should union overlapping availabilities', () => {
+    it('should union overlapping intervals', () => {
         let i1 = [[0, 10], [15, 19], [20, 28]];
         let i2 = [[4, 13], [14, 18], [25, 28]];
         let e =  [[0, 13], [14, 19], [20, 28]];
@@ -12,7 +12,7 @@ describe('unionIntervals', () => {
         expect(result).to.deep.equal(e);
     });
 
-    it('should union disjoint availabilities', () => {
+    it('should union disjoint intervals', () => {
         let i1 = [[0, 12], [14, 19], [33, 42]];
         let i2 = [[12, 14], [19, 33], [42, 50], [60, 80], [90, 100]];
         let e =  [[0, 50], [60, 80], [90, 100]];
@@ -22,7 +22,7 @@ describe('unionIntervals', () => {
         expect(result).to.deep.equal(e);
     });
 
-    it('should union 4 availabilities', () => {
+    it('should union 4 intervals', () => {
         let i1 = [[5, 8], [20, 40], [60, 75]];
         let i2 = [[3, 10], [17, 30], [50, 90]];
         let i3 = [[9, 16], [25, 42], [48, 55], [200, 400]];
@@ -34,12 +34,12 @@ describe('unionIntervals', () => {
         expect(result).to.deep.equal(e);
     });
 
-    it('should union empty availabilities', () => {
+    it('should union empty intervals', () => {
         let result = union([], []);
         expect(result).to.deep.equal([]);
     });
 
-    it('should union empty and non-empty availabilities', () => {
+    it('should union empty and non-empty intervals', () => {
         let result = union([], [[9, 10], [12, 14], [15, 18]], []);
         expect(result).to.deep.equal([[9, 10], [12, 14], [15, 18]]);
     });
